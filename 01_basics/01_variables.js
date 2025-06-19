6797493e0f214fcc1b3e5f97e70936e9bcd1153e
MohammadSaif-coder <saifali662266@gmail.com>
2025-06-19 19:34:09 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId="786786"
+let accountEmail="saifali662266@gmail.com"
+var accountPassword="667711"
+
+
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountPassword);
+let accountDetails;
+
+console.table([accountId,accountEmail,accountPassword,accountDetails]);
