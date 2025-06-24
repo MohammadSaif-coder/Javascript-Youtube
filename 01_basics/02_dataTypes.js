29c8c502c79b7534e69d2ce1a5a3ef027ddb1726
MohammadSaif-coder <saifali662266@gmail.com>
2025-06-24 10:30:13 +0000

    container congif done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict";  // treat all JS code as newer version
+
+// alert(3+3);   //  we are using nodeJS, Not browser
+
+console.log(2
+    +
+    5); // code readability should be high
+
+console.log(3+3);
+console.log("saif");
+
+//    DataTypes some of them.........
+
+let name="ali";  // String
+let age=21;      // Integer/number
+let isLoggedIn=true;             // boolean     
+
+console.log(name);
+console.log(age);
+console.log(isLoggedIn);
+
+
