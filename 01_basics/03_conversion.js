0a38645c96b901cc71d3599790df893989fabec8
MohammadSaif-coder <saifali662266@gmail.com>
2025-06-25 09:54:04 +0000

    Conversion

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -1,33 +1,67 @@
-let score="17abc"
+// let score="17abc"
 
-console.log(typeof score)
-console.log(typeof(score))
+// console.log(typeof score)
+// console.log(typeof(score))
 
-let valueInNumber=Number(score)    // Yeh string  ko number me convert kr dega......
+// let valueInNumber=Number(score)    // Yeh string  ko number me convert kr dega......
 
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
 
-//Notes.......
+// //Notes.......
 
-//"33" ko hum convert krte hain number me i.e 33 toh wo ho jataa hainn.
-// "33abc"  => NaN deta hai, i.e not a number   NaN ka type number hain
-// true => 1 , false => 0 convert hota hai..
+// //"33" ko hum convert krte hain number me i.e 33 toh wo ho jataa hainn.
+// // "33abc"  => NaN deta hai, i.e not a number   NaN ka type number hain
+// // true => 1 , false => 0 convert hota hai..
 
 
-let isLoggedIn=1
+// let isLoggedIn=1
 
-let booleanIsLoggedIn=Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
 
-// empty string " " => false
-//   "saif ali" => true
+// // empty string " " => false
+// //   "saif ali" => true
 
-// Converting number into String.........!!!
+// // Converting number into String.........!!!
 
-let someNumber=77;
-console.log(someNumber)
-console.log(typeof someNumber)
-let stringNumber=String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// let someNumber=77;
+// console.log(someNumber)
+// console.log(typeof someNumber)
+// let stringNumber=String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+
+//------------------------- Operations In Javascript --------------------------------
+
+let value=7;
+let negativeValue=-value;
+console.log(negativeValue);
+
+console.log(2+5);
+console.log(2-2);
+console.log(2*7);
+console.log(17/2);
+console.log(6%2);  // implies the remainder..!!
+console.log(2**3);
+
+let str1="Hey Handsome!!"
+let str2=" Whats going On..."
+let str3=str1+str2
+console.log(str3);
+
+//----------Yeh DEkho!! ------------
+
+console.log("1"+2);
+console.log(1+"2");
+                                      //Sirf aage peeeche ka khel hain Dost!!
+console.log("1"+2+2);
+console.log(1+2+"2")
+
+console.log(3+7*2%8)  
+// Yeh sirf exam me puchta hai,production based companies aisi cheeze nhi puchti hain 
+
+console.log((7+21) *2 %6)  // agr kisi ko jdli solve krna hain toh bracket lga do!!
+
+//Note:   Postfix and prefix pdh lo ek baar i.e  x++(post)   ++x(pre)
