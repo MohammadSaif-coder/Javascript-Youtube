b757801908f75275149faa6dc089b7aec378f0b6
MohammadSaif-coder <saifali662266@gmail.com>
2025-06-25 10:30:24 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,22 @@
+console.log(7>2);
+console.log(2<5);
+console.log(2>=1);
+console.log(7<=5);
+console.log(2==100);
+console.log(2!=7);
+
+console.log("2">1);  // 2 bda hai 1 se (true)
+console.log("5">7);  // 7 bda hai 5 se (false)
+
+console.log(null>0);       
+console.log(null==0);
+console.log(null>=0); //true 
+
+//Note ::   Undefined me saare false dete hain...
+
+
+
+// === (Yeh strictly check krta hain...)
+
+console.log("2"===2) //false .ie string & numbers can't be equal.
+
