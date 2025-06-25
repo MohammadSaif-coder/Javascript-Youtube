67f33da89b9dc67783649d2ddf79550d1843bc5f
MohammadSaif-coder <saifali662266@gmail.com>
2025-06-25 11:16:27 +0000

    Types of data

---
+++ 01_basics/DataType-summary.js
@@ -0,0 +1,39 @@
+// DataTypes are of two Types......!!
+
+//Primitive (7-types) :- Numbers,boolean,null,undefined,symbol,BigInt,string.
+//Examples are given accorgingly...
+
+const score=7;                  // Numbers
+const valueScored=100.2;
+
+const isLoggedIn=true;         // boolean ... and so on...
+
+const outsideTemp=null;
+
+let userEmail;
+
+const id=Symbol('1234567');
+
+const bigNumber=125678101895447886;
+
+const str="Mohammad saif Ali";
+
+
+
+// Non-primitive..(Reference DataTypes)
+
+// Arrays..
+const arr=["saif","kaif","hero","sher"];
+
+//Objects..
+const  myObj={
+    name:"saif",
+    age:21,
+};
+
+//Functions..
+const myFun=function(){
+    console.log("Allah Hu Akbar!!")
+}
+
+
