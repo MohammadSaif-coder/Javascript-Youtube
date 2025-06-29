75003c138ede1447a6382e1a1bd33895660eb917
MohammadSaif-coder <saifali662266@gmail.com>
2025-06-29 17:24:22 +0000

    string

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,20 @@
+const name="Mohammad Saif Ali";
+const repositryCount=51
+
+console.log(`My name is ${name} and my repositryCount are ${repositryCount}`)
+
+const myName = new String("Mohammad Saif Ali")
+
+console.log(myName.length)
+console.log(myName.toUpperCase())
+console.log(myName.toLowerCase())
+console.log(myName.charAt(7))
+console.log(myName.indexOf(4))
+
+const newString="     boomboom      "
+
+console.log(newString)
+
+console.log(newString.trim())
+
+console.log(newString.trim().toUpperCase())
