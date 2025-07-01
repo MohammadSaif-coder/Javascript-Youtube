6412dabf8d30ed4b7b1225d689b4a604dc2e7ded
MohammadSaif-coder <saifali662266@gmail.com>
2025-07-01 06:15:18 +0000

    JSON

--- 02_basics/02_object.js
+++ 02_basics/02_object.js
@@ -1,39 +1,114 @@
-// Now, Object singleton or by using constructors
+// // Now, Object singleton or by using constructors
 
-//const tinderUser=new Object()
+// //const tinderUser=new Object()
 
-//console.log(tinderUser) // empty  {}
-// Also :--
+// //console.log(tinderUser) // empty  {}
+// // Also :--
 
-//const tinderUser={}
+// //const tinderUser={}
 
-// tinderUser.id="786@ali"
-// tinderUser.name="saif"
-// tinderUser.isLoggedIn=true
+// // tinderUser.id="786@ali"
+// // tinderUser.name="saif"
+// // tinderUser.isLoggedIn=true
 
-//console.log(tinderUser)
-
-
+// //console.log(tinderUser)
 
 
-const regularUser={
-    fullName:{
-        userFullName:{
-            firstName:"saif",
-            lastName:"ali"
-        }
-    }
+
+
+// const regularUser={
+//     fullName:{
+//         userFullName:{
+//             firstName:"saif",
+//             lastName:"ali"
+//         }
+//     }
+// }
+
+// console.log(regularUser.fullName.userFullName)
+
+
+// const obj1={1:"a",2:"b"}
+// const obj2={3:"c",4:"d"}
+// const obj3={5:"e",6:"f"}
+
+// const obj4={...obj1, ...obj2, ...obj3}
+// console.log(obj4)
+
+// // *******************  Ab Dekho !! *********************
+
+// const users=[
+//     {
+//         id:1,                           // 0-index
+//         email:"ali@gmail.com"
+//     },
+
+//     {
+//         id:2,
+//         email:"hero@gmail.com"            // 1-index
+//     },
+
+//     {
+//         id:3,
+//         email:"sher@gmail.com"           // 2-index
+//     }
+
+// ] 
+
+// // To access it.... 
+
+// users[0].email
+
+// console.log(users[0].email)
+
+
+// const tinderUser={
+//     address:"gkp",
+//     name:"saif ali",
+//     rollno:52,
+//     isLoggedIn:true
+// }
+
+// console.log(tinderUser)
+
+// console.log(Object.keys(tinderUser))
+// console.log(Object.values(tinderUser))
+// console.log(Object.entries(tinderUser))
+
+
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'))
+
+// console.log(tinderUser.hasOwnProperty('isLogged'))
+
+//--------------------------------------------------------------------------------------
+
+const course={
+    courseName:"Js in Hindi",
+    coursePrice:999,
+    courseInstructor:"Boom Boom Afridi"
 }
 
-console.log(regularUser.fullName.userFullName)
+course.courseInstructor //   Boom Boom Afridi Dega.....i.e  :--
+
+console.log(course.courseInstructor) 
+
+const {courseInstructor:jiii}=course  //  Ise khete hai Object de-structrueing
+
+console.log(jiii)
 
 
-const obj1={1:"a",2:"b"}
-const obj2={3:"c",4:"d"}
-const obj3={5:"e",6:"f"}
+//--------------------- JSON Declaration Dekho ----------------
 
-//const obj4=Object.assign({},obj1,obj2,obj3)
+{
+    "name":"Boom Boom",
+    "rollno":52,
+    "email":"ali@1234567"
+}
 
-const obj4={...obj1,  ...obj2 }
 
-console.log(obj4)
+[            // Json ko aise bhi likhte hai, wo aage dekhenge..........
+    {},
+    {},
+    {}
+]
+
