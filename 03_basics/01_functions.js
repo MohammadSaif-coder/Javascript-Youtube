5eaaf75afc1d2e9a405004328561dcfcd44e3d0c
MohammadSaif-coder <saifali662266@gmail.com>
2025-07-04 16:09:30 +0000

    functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,14 @@
+function sayMyName(){
+    console.log("S")
+    console.log("A")
+    console.log("I")
+    console.log("F")
+}
+
+sayMyName()
+
+function addTwoNumbers(num1,num2){
+    console.log(num1+num2)
+}
+
+addTwoNumbers(7,2)
