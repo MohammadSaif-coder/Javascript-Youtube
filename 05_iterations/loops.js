fe1b3407058bc46313e1df244b04b854f0d7e38b
MohammadSaif-coder <saifali662266@gmail.com>
2025-07-09 11:56:49 +0000

    loopsss

---
+++ 05_iterations/loops.js
@@ -0,0 +1,87 @@
+// // // For Loop
+
+// // for (let i = 1; i <= 10; i++) {
+// //     const element = i;                 // 1 to 10 numbers
+// //     console.log(element)
+// // }
+
+// // for (let index = 1; index <=10; index++) {
+// //     const element = index*2;                   // 2 ka table
+// //     console.log(element)
+// // }
+
+// // for (let i = 0; i <= 10; i++) {
+// //     console.log(`Outer loop value: ${i}`)
+// //     for (let j = 0; j <= 10; j++) {
+// //         console.log(`Inner Loop Value: ${j} and inner loop ${i}`)
+// //          }
+    
+// // }
+
+// // for(let i=0;i<=10;i++){
+// //     console.log(`Outer Loop Value: ${i}`)
+// //     for(let j=0;j<=10;j++){
+// //          console.log(i+ '*' +j+ '=' + i*j)
+// //     }
+// // }
+
+// // // Arrays dekho..
+
+// // let arr=["saif","hero","superman"]
+// // for(let i=0;i<arr.length;i++){
+// //     const element=arr[i];
+// //     console.log(element)
+// // }
+
+// // console.log(arr.length)
+
+// // //------------ Break and Continue ------------------------
+
+// // for(let i=0;i<=20;i++){
+// //     if(i==5){
+// //         console.log("5 is Detected")
+// //         break
+// //     }
+// //     console.log(`Value of i is: ${i}`)
+// // }
+
+
+// // for(let i=0;i<=20;i++){
+// //     if(i==5){
+// //         console.log("5 is Detected")
+// //         continue
+// //     }
+// //     console.log(`Value of i is: ${i}`)
+// // }
+
+// // ----------------- while  loop --------------------------
+
+// let index=0
+
+// while(index<=10){
+//     console.log(`Value of index is ${index}`)
+//     index+=2
+// }
+
+// let array=["saif","ali","hero","babuaa"]
+
+// let arr=0
+
+// while(arr<array.length){
+//     console.log(`Value is ${array[arr]}`)
+//     arr+=1
+// }
+
+// +++++++++++++++++++++++ do while loop +++++++++++++++++++++++
+
+
+// do{
+
+// }while(condition);
+
+let score =1
+
+do{
+    console.log(`Score is ${score}`)
+    score++
+}while(score<=10)
